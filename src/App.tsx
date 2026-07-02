91560963275355155ac7121c8a45d58a3ff7ac10
etflexa <teixeira_wifi@hotmail.com>
2026-07-01 21:06:46 -0300

    configuração do tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 
-
-
 function App() {
-  
-
   return (
-    <>
-      <h1>RODANDO!</h1>
-    </>
-  )
+    <div className="flex h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        Tailwind funcionando 🚀
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
