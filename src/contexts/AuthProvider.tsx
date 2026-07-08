7a68752a42e58960e3360061b067d96a51bd5174
etflexa <teixeira_wifi@hotmail.com>
2026-07-07 22:59:18 -0300

    permanencia de sessão

--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { ReactNode } from "react";
 
 import { AuthContext } from "./AuthContext";
-
 import type { IUser } from "../types/User";
 
 import {
   signInWithPopup,
   signOut as firebaseSignOut,
+  onAuthStateChanged,
+  type User,
 } from "firebase/auth";
 
 import { auth, googleProvider } from "../firebase/firebase";
@@ -18,23 +19,46 @@ interface AuthProviderProps {
 
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<IUser | null>(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  /**
+   * Converte o usuário do Firebase para o modelo da aplicação.
+   * Assim evitamos duplicar código.
+   */
+  function mapFirebaseUser(firebaseUser: User): IUser {
+    return {
+      uid: firebaseUser.uid,
+      displayName: firebaseUser.displayName,
+      email: firebaseUser.email,
+      photoURL: firebaseUser.photoURL,
+    };
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      if (firebaseUser) {
+        setUser(mapFirebaseUser(firebaseUser));
+      } else {
+        setUser(null);
+      }
+
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+  console.log("Usuário atualizado:", user);
+}, [user]);
 
   async function signIn() {
     try {
       setLoading(true);
 
       const result = await signInWithPopup(auth, googleProvider);
-      console.log(result);
 
-      const firebaseUser = result.user;
-
-      setUser({
-        uid: firebaseUser.uid,
-        displayName: firebaseUser.displayName,
-        email: firebaseUser.email,
-        photoURL: firebaseUser.photoURL,
-      });
+      setUser(mapFirebaseUser(result.user));
     } catch (error) {
       console.error("Erro ao realizar login:", error);
     } finally {
